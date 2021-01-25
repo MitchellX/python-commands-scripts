b247595d57e4de5fd298f2434109addd23947adf
Mingcan Xiang <47625290+MitchellX@users.noreply.github.com>
2021-01-25 15:14:50 +0800

    the method is FSAnet from Github

---
+++ calculate_headpose/headpose_quantitative.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+
+root = 'headPose\\'
+datasets = ['celeba', 'ffhq', 'forensics', 'vgg2']
+
+write_log = open('results_log.txt', 'w')
+
+for dataset in datasets:
+    file_names = sorted(os.listdir(root + dataset))
+
+    for file_name in file_names:
+        with open(os.path.join(root, dataset, file_name)) as f:
+            results = f.readlines()
+            results = list(map(float, results))
+
+            results_mean = np.mean(results)
+            results_var = np.var(results)
+            results_std = np.std(results)
+
+            # write the logfile to save the results
+            write_log.write(os.path.join(root, dataset, file_name) + ":\n")
+            write_log.write("mean: " + str(results_mean) + '\n')
+            write_log.write("var: " + str(results_var) + '\n')
+            write_log.write("std: " + str(results_std) + '\n\n')
+
+write_log.close()
+
